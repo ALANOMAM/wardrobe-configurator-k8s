3564ba792cc2cbaef0ef0f2421f77a210bef7d29
alan_omam <omamalan@hotmail.it>
2025-09-08 18:21:18 +0200

    fixed app.js file in backend to allow flexible CORS management and  updated images for deploments

--- backend/app.js
+++ backend/app.js
@@ -8,15 +8,25 @@ const indexRouter = require("./routes/index");
 
 const app = express();
 
-app.use(
-  cors({
-    origin: "http://localhost:3000", // Allow only frontend origin (IN DEV MODE)
-    //origin: "http://192.168.49.2:30080", // Allow only frontend origin (MINIKUBE FRONTEND NODE PORT)
-    credentials: true, // if you're using cookies or auth headers
-  })
-);
+// Logic that makes me switch between the two frontend origins START
+const whitelist = [
+  "http://localhost:3000", // Allow only frontend origin (IN DEV MODE)
+  "http://192.168.49.2:30080", // Allow only frontend origin (MINIKUBE FRONTEND NODE PORT)
+];
 
-//app.use(cors()); // Allow all origins (good for dev, but not for production)
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || whitelist.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+// Logic that makes me switch between the two frontend origins END
 
 app.use(logger("dev"));
 app.use(express.json());
